cb950da3d5a97a79210b7bb93a318dcddb59667a
Gillesedgy <edgygilles@pursuit.org>
2023-03-27 13:18:59 -0400

    insert and size created

--- solution.js
+++ solution.js
@@ -1,19 +1,48 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+// define linkList with the head property ...
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  // insert
+  insert(data) {
+    let nodeOne = new Node(data); // create an instance => new node
+    if (!this.head) {
+      this.head = nodeOne;
+    } else {
+      nodeOne.next = this.head;
+      this.head = nodeOne;
+    }
+  }
+  // Count 
+  size(){
+    let count = 0 
+    let node = this.head
+    while(node){
+      count++
+     node = node.next
+
+    }
+    return count
+    
+  }
+
+  isEmpty() {
+    return this.head === null;
+  }
+  clear() {
+    this.head === null;
+  }
+}
+
 module.exports = {
   Node,
   LinkedList,
 };
-
-class Node {
-  constructor(data){
-this.data = data;
-this.next = null
-  }
-}
-
-class LinkList{
-  constructor(){
-    this.head = null
-  }
-}
