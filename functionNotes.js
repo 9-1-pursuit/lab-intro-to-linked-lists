0fd628d7da679396e930f0643ff6292c0fe13add
DestinyJoyner <destinyjoyner@pursuit.org>
2023-03-26 22:37:15 -0400

    function comment file created

---
+++ functionNotes.js
@@ -0,0 +1,52 @@
+// INSERT
+function insert(data){
+    // create a new node with given data
+    let newNode = new Node(data)
+    // if linked list is empty (head = null), set the newNode to the head
+    if(!this.head) this.head = newNode
+    else {
+        // initiate current as the head node, and when loop get to tail node (.next = null) set the tail.next to the newNode (add to end of list) 
+      let current = this.head
+      while(current.next){
+        // up until .next = null (the last (tail) node)
+        current = current.next
+      }
+      current.next = newNode
+    }
+  }
+
+// SIZE
+  function size(){
+    // start at the head count is 0 
+    let current = this.head
+    let count = 0
+    while(current) {
+      count++
+    //   update current to the next node , if current.next is null (tail), return count
+      current = current.next
+    }
+    return count
+  }
+
+//   DELETE
+function deleteNode(index){
+    let current = this.head
+    let count = 0
+    /* 
+    if delete at index 2
+      store previous and current.next nodes (prev = 1, curr.next =3)
+      when index = count (prev.next = curr.next, 1.next = 3)
+      */
+    let prevNode = null
+    // if index = 0, head, set head to null
+    if(index === 0) this.head = null
+    else {
+      while(count < index){
+        // up until index
+        prevNode = current
+        current = current.next
+        count++
+      }
+      prevNode.next = current.next
+    }
+  }
--- solution.js
+++ solution.js
@@ -12,23 +12,16 @@ class Node {
 class LinkedList {
   constructor(head = null){
     this.head = head 
-    // this.size = this.size()
   }
-  // insert
   insert(data){
     let newNode = new Node(data)
-
     if(!this.head) this.head = newNode
     else {
       let current = this.head
-      while(current.next){
-        // up until .next = null (the last (tail) node)
-        current = current.next
-      }
+      while(current.next) current = current.next
       current.next = newNode
     }
   }
-  // size
   size(){
     let current = this.head
     let count = 0
@@ -42,16 +35,10 @@ class LinkedList {
   delete(index){
     let current = this.head
     let count = 0
-    /* 
-      store previous and current.next nodes (prev = 1, curr.next =3)
-      when index = count (prev.next = curr.next, 1.next = 3)
-      */
     let prevNode = null
-    // first index
     if(index === 0) this.head = null
     else {
       while(count < index){
-        // up until index
         prevNode = current
         current = current.next
         count++
