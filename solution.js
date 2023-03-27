8074470ec90a0ccf2e7eedde0a3880510f555152
DestinyJoyner <destinyjoyner@pursuit.org>
2023-03-26 23:45:12 -0400

    cleaned up comments

--- solution.js
+++ solution.js
@@ -1,5 +1,13 @@
 const { nums, words } = require("./data/data.js");
-const { insert, size, deleteNode, search, getFirst, getLast, getKth } = require("./functionNotes")
+const { 
+  insert, 
+  size, 
+  deleteNode, 
+  search, 
+  getFirst, 
+  getLast, 
+  getKth,
+ } = require("./functionNotes")
 
 // Create a Node class with properties
 class Node {
@@ -23,28 +31,9 @@ class LinkedList {
   }
  
 }
-// const node1 = new Node(1, new Node(2))
-// const linked1 = new LinkedList(node1)
-// console.log(linked1)
-// console.log(linked1.search(1))
 
 module.exports = {
   Node,
   LinkedList,
 };
 
-
-/* 
-  insert
-size
-delete by key
-getFirst element
-getLast element
-search an element by key and return data
-getKth retrieve kth element
-getKthToLast retrieve kth from last element
-isEmpty check if list is empty
-clear the linked list
-toArrayconvert data from linked list to an array
-containsDuplicates check for duplicates - return true if contains duplicates, false if not
-*/
