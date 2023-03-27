677d5b19c4229b7fc4bc25ff195d420585796ae1
Gillesedgy <edgygilles@pursuit.org>
2023-03-27 13:53:26 -0400

    get first, getlast,search, getkth, getktlLast done

--- solution.js
+++ solution.js
@@ -21,17 +21,76 @@ class LinkedList {
       this.head = nodeOne;
     }
   }
-  // Count 
-  size(){
-    let count = 0 
-    let node = this.head
-    while(node){
-      count++
-     node = node.next
-
+  // Count
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
     }
-    return count
-    
+    return count;
+  }
+  delete(data) {
+    let node = this.head;
+    let counter = 0;
+    while (node.data !== data && node.next) {
+      counter++;
+      node = node.next;
+    }
+    let foundNode = node;
+    node = this.head;
+    for (let i = 1; i < counter; i++) {
+      node = node.next;
+    }
+    node.next = foundNode.next;
+  }
+  getFirst() {
+    return this.head;
+  }
+  getLast() {
+    if (!this.head) return null;
+    let node = this.head; //current
+    while (node.next) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+    return node;
+  }
+  search(key) {
+    let node = this.head; //current
+    while (node !== null && node.data !== key) {
+      node = node.next;
+    }
+    return node;
+  }
+  getKth(key) {
+    //current
+    let node = this.head;
+    let count = 1;
+    while (node !== null && count < key) {
+      node = node.next;
+      count++;
+    }
+    return node;
+  }
+  getKthToLast(key) {
+    if (key < 1) return null;
+    let p1 = this.head;
+    let p2 = this.head;
+    for (let i = 0; i < key; i++) {
+      if (!p2) {
+        return null;
+      }
+      p2 = p2.next;
+    }
+    while (p2.next) {
+      p2 = p2.next;
+      p1 = p1.next;
+    }
+    return p1;
   }
 
   isEmpty() {
