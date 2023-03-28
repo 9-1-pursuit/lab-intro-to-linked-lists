38abb6b98cd845c417b71b40079580e0f4be57b7
PESolut <jahaadpetty@pursuit.org>
2023-03-28 15:00:51 -0400

    linked list class created with two methods: size and insert

--- solution.js
+++ solution.js
@@ -1,5 +1,48 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data){
+    this.data = data
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor(head = null){
+    this.head = head
+  }
+  insert(data) {
+    let newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+  size(){
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+}
+
+const firstNode = new Node (words[0])
+console.log(firstNode)
+const linkedList1 = new LinkedList (firstNode)
+
+console.log(linkedList1)
+
+linkedList1.insert(words[1])
+
+console.log(linkedList1)
+
+
+
 module.exports = {
   Node,
   LinkedList,
