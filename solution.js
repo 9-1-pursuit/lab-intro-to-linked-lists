bef1f83d2b284c40efe75088a8f9d4ed0f468197
Lennie Nurse <lnursej@gmail.com>
2023-03-30 22:03:06 -0400

    Updated 13/14

--- solution.js
+++ solution.js
@@ -11,6 +11,8 @@ class Node {
 
 // creating class called LinkedList with constructor function that takes node as argument.
 // The constructor sets the head node of the LinkedList to the argument passed in(or null if no arguments is passed in.)
+
+// create linkedlist head
 class LinkedList {
   constructor(head = null) {
     this.head = head
@@ -19,6 +21,7 @@ class LinkedList {
   // Creating new node at the beginnging of a Linked List. It creates new node with data passed in as argument/parameter.
   // set the head of list to new node if no head exist in the list. Sets the next node of the new node to the current head,
 
+  // insert link list
   insert(data) {
     let newNode = new Node(data)
     if (!this.head) {
@@ -35,6 +38,7 @@ class LinkedList {
   // increments the count variable by one for each node and then sets the current node to the next node in the list.
   // When loop ends, the function returns the count variable, which is the number of nodes in the list.
 
+  // size of linked list
   size() {
     let count = 0
     let curr = this.head
@@ -45,6 +49,7 @@ class LinkedList {
     return count
   }
 
+  // delete by key
   delete(data) {
     let curr = this.head
     let count = 0
@@ -60,10 +65,12 @@ class LinkedList {
     curr.next = foundNode.next
   }
 
+  // getFirst element
   getFirst() {
     return this.head
   }
 
+  // getLast element
   getLast() {
     let curr = this.head
     if (!this.head) return null
@@ -73,6 +80,7 @@ class LinkedList {
     return curr
   }
 
+  // an element by key and return data
   search(key) {
     let curr = this.head
     while (curr !== null && curr.data !== key) {
@@ -81,44 +89,63 @@ class LinkedList {
     return curr
   }
 
+  // retrieve kth element
   getKth(key) {
     let count = 1
     let curr = this.head
-    while (curr < key) {
+    while (curr < key && count !== key) {
       curr = curr.next
       count++
     }
     return curr
   }
 
-  getKthToLast() {}
+  //retrieve kth from last element
+  getKthToLast(key) {
+    let count = 1
+    let curr = this.head
+    let size = this.size() - key
 
+    while (count !== size) {
+      curr = curr.next
+      count++
+    }
+    return curr
+  }
+
+  // check if list is empty
   isEmpty() {
     return this.head === null
   }
 
+  // clear the linked list
   clear() {
     this.head = null
   }
 
+  // convert data from linked list to an array
   toArray() {
+    const arr = []
     let curr = this.head
-    let arr = []
     while (curr) {
       arr.push(curr.data)
       curr = curr.next
     }
+
     return arr
   }
 
+  // check for duplicates - return true if contains duplicates, false if not
   containsDuplicates() {
     let curr = this.head
     let set = new Set()
-    while (curr) {
+    while (curr !== null) {
       // set.has check for a same value in the Set()
       if (set.has(curr.data)) {
         return true
       }
+      // adding the value of the curr node to the Set, so that it can be checked for duplicates
+      set.add(curr.data)
       curr = curr.next
     }
     return false
